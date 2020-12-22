6c925ebd1f331dd4dee8513767a9e176c386e924
DavidBThomp <65555644+DavidBThomp@users.noreply.github.com>
2020-12-22 11:58:39 -0600

    JavaScript Let and const

---
+++ Front-End Treehouse/JS/JavaScript Var/let-const.js
@@ -0,0 +1,10 @@
+
+const number = 5;
+// You cannot change the value of a constant or reassign the constant.
+console.log(number);
+
+let score = 10;
+score += 10;
+console.log(score)
+// You won't be allowed ot overwrite the let score later in the program
+// Javascript doesn't care to much about spacing and indents, but I have been using standard practice of between operations
