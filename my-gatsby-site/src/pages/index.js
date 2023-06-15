63ba29255cc313288d772e4fc3d73bbfe2753b13
David Thompson <davidbthomp@gmail.com>
2023-06-15 15:47:21 -0500

    Layout Component

--- my-gatsby-site/src/pages/index.js
+++ my-gatsby-site/src/pages/index.js
@@ -1,18 +1,17 @@
 // Step 1: Import React
 import * as React from 'react';
+import Layout from '../components/layout'
 
 // When linking between pages on site
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/about">About</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+    <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   )
 };
 
